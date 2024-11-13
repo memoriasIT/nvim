40d1a3e71e02282bcf7a24fe6661ec1478a36ba1
memoriasit <memoriasdeuninformatico2017@gmail.com>
2024-11-13 21:56:35 +0100

    Mappings clean up

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,23 +1,64 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 local alias = require("custom.utils").add_alias
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+-- ┓ ┏•   ┓                            
+-- ┃┃┃┓┏┓┏┫┏┓┓┏┏  ┏┳┓┏┓┏┓┏┓┏┓┏┓┏┳┓┏┓┏┓╋
+-- ┗┻┛┗┛┗┗┻┗┛┗┻┛  ┛┗┗┗┻┛┗┗┻┗┫┗ ┛┗┗┗ ┛┗┗
+-- WINDOW MANAGEMENT        ┛
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- Resize window using <shift> arrow keys
+map("n", "<S-Up>", "<cmd>resize +2<CR>")
+map("n", "<S-Down>", "<cmd>resize -2<CR>")
+map("n", "<S-Left>", "<cmd>vertical resize -2<CR>")
+map("n", "<S-Right>", "<cmd>vertical resize +2<CR>")
+
 -- Better window navigation
 map("n", "<C-h>", "<C-w>h", { desc = "Move window left" })
 map("n", "<C-j>", "<C-w>j", { desc = "Move window down" })
 map("n", "<C-k>", "<C-w>k", { desc = "Move window up" })
 map("n", "<C-l>", "<C-w>l", { desc = "Move window right" })
 
--- buffer go next and prev
---map("n", "bh", ":bprev<CR>", { desc = "Go to prev buffer" })
---map("n", "bl", ":bnext<CR>", { desc = "Go to next buffer" })
+
+-- ┳┳┓   •     
+-- ┃┃┃┏┓╋┓┏┓┏┓┏
+-- ┛ ┗┗┛┗┗┗┛┛┗┛
+-- MOTIONS
+
+-- Scrolling
+map("n", "<C-d>", "<C-d>zz", { desc = "Scroll down and center" })
+map("n", "<C-u>", "<C-u>zz", { desc = "Scroll up and center" })
+
+
+-- ┏┓┓•  ┓        ┓
+-- ┃ ┃┓┏┓┣┓┏┓┏┓┏┓┏┫
+-- ┗┛┗┗┣┛┗┛┗┛┗┻┛ ┗┻
+--     ┛  CLIPBOARD
+
+-- Use <leader>+C and <leader>+V to copy/paste to the system clipboard
+-- Enable clipboard support if it's not already enabled
+vim.opt.clipboard = "unnamedplus"
+vim.keymap.set("v", "<leader>c", '"+y', { desc = "Copy to system clipboard" })
+vim.keymap.set("n", "<leader>v", '"+p', { desc = "Paste from system clipboard" })
+vim.keymap.set("v", "<leader>v", '"+p', { desc = "Paste from system clipboard" })
+
+-- Paste over currently selected text without yanking it
+map("v", "p", '"_dp')
+
+
+-- ┏┓    ┓•       ┏  ┓ •┏  
+-- ┃┃┓┏┏┓┃┓╋┓┏  ┏┓╋  ┃ ┓╋┏┓
+-- ┗┻┗┻┗┻┗┗┗┗┫  ┗┛┛  ┗┛┗┛┗ 
+-- Quality   ┛   of    Life                      
+
+
+-- Enter command with ; (dumb fingers-proof)
+map("n", ";", ":", { desc = "CMD enter command mode" })
+
+-- Go to (N)ormal mode from (I)nsert with jk
+map("i", "jk", "<ESC>")
+
 
 -- Workarounds for my dumb fingers
 -- Credits to https://github.com/mgastonportillo/nvchad-config/blob/main/lua/gale/aliases.lua
@@ -26,9 +67,7 @@ alias("qa", "QA")
 alias("q", "Q")
 alias("w", "W")
 
--- Use <leader>+C and <leader>+V to copy/paste to the system clipboard
--- Enable clipboard support if it's not already enabled
-vim.opt.clipboard = "unnamedplus"
-vim.keymap.set("v", "<leader>c", '"+y', { desc = "Copy to system clipboard" })
-vim.keymap.set("n", "<leader>v", '"+p', { desc = "Paste from system clipboard" })
-vim.keymap.set("v", "<leader>v", '"+p', { desc = "Paste from system clipboard" })
+-- Better indent
+map("v", "<", "<gv", { desc = "Indent left" })
+map("v", ">", ">gv", { desc = "Indent right" })
+
